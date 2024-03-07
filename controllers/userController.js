ad7c4491c4dac7bd710723d16b276c4d52ef5956
Anthony Blanco <anthony200527@outlook.com>
2024-03-06 16:57:06 -0800

    Friend routes for add and remove!

--- controllers/userController.js
+++ controllers/userController.js
@@ -54,9 +54,34 @@ module.exports = {
       if (!user) {
         return res.status(404).json({ message: 'No user with that ID' });
       } else {
-        return res.status(200).json(user, "User Deleted")
+        return res.status(200).json(user)
       }
     } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async addFriend (req, res) {
+    try{
+      const user = await User.updateOne({ _id: req.params.userId }, { $push: { friends: req.params.friendId}})
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      } else {
+        return res.status(200).json(user)
+      }
+    } catch (err){
+      res.status(500).json(err);
+    }
+  },
+  async removeFriend (req, res) {
+    try{
+      const user = await User.updateOne({ _id: req.params.userId }, { $pullAll: { friends: [{_id: req.params.friendId}]}})
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      } else {
+        return res.status(200).json(user)
+      }
+    } catch (err){
+      console.log(err)
       res.status(500).json(err);
     }
   }
